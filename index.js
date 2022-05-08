9841cb566ce5e5b77eac57b5d61af45dd75da38c
shamsulhuda <shamsulhuda310@gmail.com>
2022-05-08 13:38:38 +0600

    parsing request

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ Date: 25/04/2022
 
 // Dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 
@@ -30,8 +32,26 @@ app.createServer = () => {
 
 // handle request response
 app.handleReqRes = (req, res) => {
-    // response handle
-    res.end('Hello Programmers');
+    // request url
+    // parse the url
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimedPth = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        // response handle
+        res.end('Hello Programmers');
+    });
 };
 
 // Start server
